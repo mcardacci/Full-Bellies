91969ccbaa0ab1af1f9a901f4840ab23ab88744b
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-07-17 19:26:50 -0400

    change parseInt to parseFloat

--- app/assets/javascripts/updateprice.js
+++ app/assets/javascripts/updateprice.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   $('#purchaseitemquantity').change(function(){
-    var $price = parseInt($('#dealprice').text()) * parseInt($('#purchaseitemquantity').val());
+    var $price = parseFloat($('#dealprice').text()) * parseFloat($('#purchaseitemquantity').val());
     $price = $price.toString();
+
     $('#purchaseitemprice').html($price);
   })
 })
