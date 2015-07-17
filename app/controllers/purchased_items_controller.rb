809086d11855ca12989108dab122a3201b066720
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-07-17 16:58:31 -0400

    work on create route

--- app/controllers/purchased_items_controller.rb
+++ app/controllers/purchased_items_controller.rb
@@ -5,5 +5,30 @@ class PurchasedItemsController < ApplicationController
   end
 
   def create
+    deal = Deal.find(params[:deal_id])
+
+    binding.pry
+    @amount = 500
+
+    customer = Stripe::Customer.create(
+      :email => 'example@stripe.com',
+      :card  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
   end
+
+  private
+
+
+  def calculate_price(quantity, deal)
+    return quantity * deal.price
+  end
+
+
 end
