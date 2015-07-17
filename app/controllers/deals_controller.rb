fda9b0842a146da63b223d1bb302182c40abf2b5
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-07-17 07:15:34 -0400

    created deals controller with crud routes

---
+++ app/controllers/deals_controller.rb
@@ -0,0 +1,38 @@
+class DealsController < ApplicationController
+
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def deal_params
+      params.require().permit()
+    end
+
+
+end
