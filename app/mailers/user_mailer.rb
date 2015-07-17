8c701be08f3f73901c7bf2da3b034b59087ce0bd
Lowell Mower <lowell.m.mower@gmail.com>
2015-07-17 19:24:48 -0400

    each loop for mailer for multiple mailings

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,11 @@
 class UserMailer < ApplicationMailer
 
   default from: 'lowell.mower@gmail.com'
-  def welcome_email(user)
-    @user = user
-    @url  = 'https://afternoon-meadow-3523.herokuapp.com/'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  def welcome_email(users)
+    users.each do |user|
+      @user = user
+      @url  = 'https://afternoon-meadow-3523.herokuapp.com/'
+      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    end
   end
 end
