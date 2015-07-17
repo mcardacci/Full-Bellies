1db27eb6f507f46400c9eec72db2a2f86a1ccc7e
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-07-17 17:54:44 -0400

    remove debugger

--- app/assets/javascripts/updateprice.js
+++ app/assets/javascripts/updateprice.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   $('#purchaseitemquantity').change(function(){
     var $price = parseInt($('#dealprice').text()) * parseInt($('#purchaseitemquantity').val());
-    debugger
     $price = $price.toString();
     $('#purchaseitemprice').html($price);
   })
