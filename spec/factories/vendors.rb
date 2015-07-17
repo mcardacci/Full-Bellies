c57e86e37fb8f69c18005c483daff9d7872d65c7
John Lyden <john.lyden86@gmail.com>
2015-07-17 12:07:14 -0400

    adding vendor factory

---
+++ spec/factories/vendors.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :vendor do
+    username {Faker::Internet.user_name}
+    email {Faker::Internet.email}
+    password "123"
+    phone_number {Faker::PhoneNumber.phone_number}
+    profile_pic {Faker::Avatar.image}
+    name {Faker::Company.name}
+    address {Faker::Address.street_address}
+    bio {Faker::Company.catch_phrase}
+
+
+    factory :invalid_vendor do
+      username nil
+    end
+  end
+end
