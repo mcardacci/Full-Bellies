dc5a8748e9dcf23b4da937504f87d47b95138eb4
mcardacci <marc.cardacci@gmail.com>
2015-07-17 15:14:25 -0400

    fixes new user routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,7 @@ class UserController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to user_path(@user.id)
     else
       flash[:notice] = "There was a problem with your submission"
       redirect_to new_user_path
