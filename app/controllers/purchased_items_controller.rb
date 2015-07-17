ee1ef90849f0096f2ed7f198eb40fdb066ef117c
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-07-17 17:55:05 -0400

    creat purchased items

--- app/controllers/purchased_items_controller.rb
+++ app/controllers/purchased_items_controller.rb
@@ -6,9 +6,11 @@ class PurchasedItemsController < ApplicationController
 
   def create
     deal = Deal.find(params[:deal_id])
+    purchased_item = PurchasedItem.create(purchased_item_params)
+    purchased_item.update_attributes(price: calculate_price(params[:quantity], deal), user_id: current_user.id)
 
     binding.pry
-    @amount = 500
+
 
     customer = Stripe::Customer.create(
       :email => 'example@stripe.com',
@@ -17,17 +19,25 @@ class PurchasedItemsController < ApplicationController
 
     charge = Stripe::Charge.create(
       :customer    => customer.id,
-      :amount      => @amount,
+      :amount      => calculate_price(params[:quantity], deal),
       :description => 'Rails Stripe customer',
       :currency    => 'usd'
     )
+    redirect_to user_path(current_user.id)
   end
 
   private
 
+  def purchased_item_params
+    params.permit(:deal_id, :title,:description,:quantity)
+  end
 
   def calculate_price(quantity, deal)
-    return quantity * deal.price
+    return quantity * deal.item_price
+  end
+
+  def current_user
+    return User.find(session[:user_id]) if !!session[:user_id]
   end
 
 
