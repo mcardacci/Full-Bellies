8593e8e103b6fe8d6e96755c185c31ffe0328075
johnlyden <john.lyden6@gmail.com>
2015-07-16 20:23:59 -0400

    adding edit, update, destroy

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -18,6 +18,27 @@ class UserController < ApplicationController
     @user = User.find(params[:id])
   end
 
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(user_params)
+    if @user.save
+      redirect_to root_path, flash[:notice] = "Successfully edited account"
+    else
+      render :edit
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
   private
 
   def user_params
