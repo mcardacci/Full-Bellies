eaaf556276c5ae19967642844ec61fc738a3b373
John Lyden <john.lyden86@gmail.com>
2015-07-17 12:40:28 -0400

    fixing syntax on user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,7 +15,7 @@ describe User do
     expect(build(:user)).to be_valid
   end
   it "is invalid without a username" do
-    user = FactoryGirl.build(:user, username: nil)
+    user = build(:user, username: nil)
     user.valid?
     expect(user.errors[:username]).to include("can't be blank")
   end
@@ -28,13 +28,15 @@ describe User do
 
   it "is invalid with a duplicate email address" do
     create(:user, email: "test@example.com")
-    user = build(:user, email: "test@example")
+    user = build(:user, email: "test@example.com")
+    user.valid?
     expect(user.errors[:email]).to include('has already been taken')
   end
 
   it "is invalid with a duplicate username" do
     create(:user, username: "bob")
     user = build(:user, username: "bob")
+    user.valid?
     expect(user.errors[:username]).to include('has already been taken')
   end
 end
