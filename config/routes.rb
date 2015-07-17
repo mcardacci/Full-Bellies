9a810a83a426548c0fcb697aabe016247a51c62d
Lowell Mower <lowell.m.mower@gmail.com>
2015-07-16 21:51:53 -0400

    add single route for session new

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :sessions, only: [:new]
+
   get '/login' => 'sessions#new'
   post '/login_user' => 'sessions#user_create'
 
