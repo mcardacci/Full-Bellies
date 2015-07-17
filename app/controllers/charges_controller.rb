630265b33aa0db8d683a3250cb4b80d6205a607d
Marc Cardacci <marc.cardacci@gmail.com>
2015-07-17 11:41:56 -0400

    Update charges_controller.rb

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,26 +1,26 @@
 class ChargesController < ApplicationController
 
-	def index
-	  @price_header = "($00.50)"
-	  @price_btn = 50
-	end
+  def index
+    @price_header = "($00.50)"
+    @price_btn = 50	
+  end
 
-	def create
-	# set hidden field in form and capture the @amount in params
-	  @amount = 500
-	  
-	  customer = Stripe::Customer.create(
-	    :email => 'example@stripe.com',
-	    :card  => params[:stripeToken]
-	  )
+  def create
+    # set hidden field in form and capture the @amount in params
+    @amount = 500
+    
+    customer = Stripe::Customer.create(
+      :email => 'example@stripe.com',
+      :card  => params[:stripeToken]
+    )
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => @amount,
-	    :description => 'Rails Stripe customer',
-	    :currency    => 'usd'
-	  )
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
 	  
-	  redirect_to deals_path
+    redirect_to deals_path
   end
 end
