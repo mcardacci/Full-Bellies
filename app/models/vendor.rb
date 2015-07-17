c5e5aafeed897b4a83999c3578847f8d69b7b8e9
John Lyden <john.lyden86@gmail.com>
2015-07-17 12:25:12 -0400

    cleaning up user and vendor validations

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -7,7 +7,6 @@ class Vendor < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
-  validates :password_digest, presence: true
   validates :address, presence: true
 
   def send_followers_sms
