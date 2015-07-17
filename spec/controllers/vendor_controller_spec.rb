1ee706604d25932729a68f4688e2f309e38021ac
mcardacci <marc.cardacci@gmail.com>
2015-07-17 19:16:46 -0400

    must pull from master

--- spec/controllers/vendor_controller_spec.rb
+++ spec/controllers/vendor_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-describe VendorContrller do
+describe VendorsController do
 	describe 'vendor access' do
 		before :each do
 			@vendor = create(:vendor)
 			session[:vendor_id] = @vendor.id
 		end
-		
+	end
 end
