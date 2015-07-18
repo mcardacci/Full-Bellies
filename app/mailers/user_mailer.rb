6e52ea0fae3001c67a302dd54e3adbd86a504968
Lowell Mower <lowell.m.mower@gmail.com>
2015-07-18 14:10:56 -0400

    small changes to email controller

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
 
   def email_followers(user_emails)
     @url  = 'https://afternoon-meadow-3523.herokuapp.com/'
-    mail(to: user_emails, subject: "You're purchase has been placed")
+    mail(to: user_emails, subject: "New Deals Have Been Posted")
   end
 end
