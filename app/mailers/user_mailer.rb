952e0ebda6aa3b11c65befd5229ea5c45ac6843c
Lowell Mower <lowell.m.mower@gmail.com>
2015-07-17 20:27:39 -0400

    add ability for multi email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,9 @@
 class UserMailer < ApplicationMailer
 
   default from: 'lowell.mower@gmail.com'
-  def welcome_email(users)
-    users.each do |user|
-      @user = user
-      @url  = 'https://afternoon-meadow-3523.herokuapp.com/'
-      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-    end
+
+  def email_followers(user_emails)
+    @url  = 'https://afternoon-meadow-3523.herokuapp.com/'
+    mail(to: user_emails, subject: 'Welcome to My Awesome Site')
   end
 end
