ca1e28d51fa306efc44f75cabf28384c9b366c97
Marc Cardacci <marc.cardacci@gmail.com>
2015-07-18 14:17:29 -0400

    adds unfinished follow and unfollow

--- app/assets/javascripts/followVendor.js
+++ app/assets/javascripts/followVendor.js
@@ -1,7 +1,36 @@
 $(document).ready(function(){
-  var updateVendorFollowers = $('.follow-button').on('click', follow)
-  var decrementVendorFollowers;
-})
+  $('.follow-button').on('click', follow);
+  $('.unfollow-button').on('click', unfollow);
+});
 
+var follow = function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  var $target = $(event.target);
+  var route = $target.attr('href');
+  $.ajax({
+    url : route,
+    method : 'PUT',
+    dataType : 'json'
+  }).done(function(response) {
+    $('.follower-count').html(response.upvote);
+  }).fail(function(error) {
+    console.log("messed up");
+  })
+};
 
-
+var unfollow = function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  var $target = $(event.target);
+  var route = $target.attr('href');
+  $.ajax({
+    url : route,
+    method : 'DELETE',
+    dataType : 'json'
+  }).done(function(response) {
+    $('.follower-count').html(response.upvote);
+  }).fail(function(error) {
+    console.log("messed up");
+  })
+};
