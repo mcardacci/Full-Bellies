362d6332e54ae59d258c3e48a2be83f7d53e9a74
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-07-18 18:10:58 -0400

    create a helper method for flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
   def current_vendors_page?(vendor)
     current_vendor == vendor
   end
+
+  def bootstrap_class_for(flash_type)
+    { "success"=> "alert-success", "error" => "alert-danger", "alert" => "alert-warning", "notice" => "alert-info" }[flash_type] || flash_type.to_s
+  end
 end
