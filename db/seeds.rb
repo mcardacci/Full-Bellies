4dbac6d71ae0b277c98cbf15180dc9634d96c70f
johnlyden <john.lyden6@gmail.com>
2015-07-18 19:37:28 -0400

    adding seed data with 5 vendors, 7 deals and followers for each vendor

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ vendor1 = Vendor.create(
   email: "ross@example.com",
   name: "ross's pizza",
   bio: "Casual spot serving pizzas, family-style pasta & Italian favorites, with outdoor seating.",
-  address: "2 Gold St, New York, NY 10038"
+  address: "2 Gold St, New York, NY 10038",
   phone_number: Faker::PhoneNumber.phone_number
   )
 
@@ -38,6 +38,16 @@ vendor4 = Vendor.create(
   name: "John's Burgers",
   bio: "Burger shop with long list of options also serving hot dogs in a basic, bright corner space.",
   address: "77 Pearl St #6, New York, NY 10004",
+  phone_number: Faker::PhoneNumber.phone_number
+  )
+
+vendor5 = Vendor.create(
+  username: "Bob",
+  password: "123",
+  email: "bob@example.com",
+  name: "Bob's Tacos",
+  bio: "Chef Bob Bloomfield's take on upscale Mexican street food in a vibrant, colorful setting.",
+  address: "145 E 39th St, New York, NY 10016",
   phone_number: Faker::PhoneNumber.phone_number
   )
 
@@ -78,5 +88,63 @@ end
     profile_pic:  Faker::Avatar.image)
 end
 
+10.times do
+  vendor4.users.create(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.safe_email,
+    password: Faker::Internet.password,
+    phone_number: Faker::PhoneNumber.phone_number,
+    profile_pic:  Faker::Avatar.image)
+end
+
 ### Deals Data
+vendor1.deals.create(
+  title: "10 delicious cheese pizzas",
+  description: "These pizzas are good and about to be trashed.  Come grab one at 90% discount",
+  item_quantity: 10,
+  item_price: 1.00,
+  end_time: Time.local(2016.to_i, 1.to_i, 1.to_i, 00.to_i, 00.to_i),
+  )
+
+vendor1.deals.create(
+  title: "20 delicious bread sticks",
+  description: "These bread sticks ain't gonna eat themselves and we don't wanna waste em!",
+  item_quantity: 10,
+  item_price: 0.50,
+  end_time: Time.local(2016.to_i, 1.to_i, 1.to_i, 00.to_i, 00.to_i),
+  )
+
+vendor2.deals.create(
+  title: "30 day old bagels",
+  description: "Bagels were made fresh yesterday and are available for .50 until the end of the day",
+  item_quantity: 30,
+  item_price: 0.50,
+  end_time: Time.local(2016.to_i, 1.to_i, 1.to_i, 00.to_i, 00.to_i),
+  )
+
+vendor3.deals.create(
+  title: "15 servings of Lasagna from lunch today",
+  description: "Some of the best lasagna in New York is leftover from our dinner service - available til close tonight",
+  item_quantity: 15,
+  item_price: 4.00,
+  end_time: Time.local(2016.to_i, 1.to_i, 1.to_i, 00.to_i, 00.to_i),
+  )
+
+vendor4.deals.create(
+  title: "25 fresh kobe beef burgers going quick!",
+  description: "Today is your lucky day, we have 25 of the best burgers in NYC available for $3.00 each",
+  item_quantity: 25,
+  item_price: 3.00,
+  end_time: Time.local(2016.to_i, 1.to_i, 1.to_i, 00.to_i, 00.to_i),
+  )
+
+vendor5.deals.create(
+  title: "22 delicoous tacos de pescado going quick!",
+  description: "Don't miss dis fish! We have 2w of the best fish Tacos in Manhattan available for $.75 each",
+  item_quantity: 25,
+  item_price: 0.75,
+  end_time: Time.local(2016.to_i, 1.to_i, 1.to_i, 00.to_i, 00.to_i),
+  )
+
+
 
