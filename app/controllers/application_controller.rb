2cbb029aaa834e88b71a8919457bdab47034ea75
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-07-18 13:01:46 -0400

    add controller methods for login checks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def vendor_logged_in?
+    !!session[:vendor_id]
+  end
+
+  def user_logged_in?
+    !!session[:user_id]
+  end
+
+  def require_vendor_login
+    unless vendor_logged_in?
+      flash[:notice] = "You must be logged in as a vendor to access that."
+      redirect_to deals_path
+    end
+  end
+
+  def require_user_login
+    unless user_logged_in?
+      flash[:notice] = "Must be logged in as a user to access that."
+      redirect_to deals_path
+    end
+  end
 end
