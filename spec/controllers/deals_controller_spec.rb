d2c6bb523d69dcb05ba41aa0038cc750312722e7
johnlyden <john.lyden6@gmail.com>
2015-07-18 12:08:11 -0400

    finising deals create tests

--- spec/controllers/deals_controller_spec.rb
+++ spec/controllers/deals_controller_spec.rb
@@ -11,23 +11,59 @@ describe DealsController do
       get :index
       expect(assigns(:deals)).to match_array [@deal, deal]
     end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+
+    it "orders the deals in descending order" do
+      deal = create(:deal)
+      get :index
+      expect(Deal.order('created_at desc')).to eq [deal, @deal]
+    end
+  end
+
+  describe '#new' do
+    it "assigns a new deal to @deal" do
+      get :new
+      expect(assigns(:deal)).to be_a_new(Deal)
+    end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "POST #create" do
+    before(:each) do
+      @vendor = create(:vendor)
+      session[:vendor_id] = @vendor.id
+    end
+
+    context "with valid attributes" do
+      it "saves the new deal into the database" do
+        expect{ post :create, deal: attributes_for(:deal, vendor_id: @vendor.id)}.to change(Deal, :count).by(1)
+      end
+
+      it "redirects to the current vendor's profile page" do
+        post :create, deal: attributes_for(:deal, vendor_id: @vendor.id)
+        expect(response).to redirect_to vendor_path(@vendor)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the deal in to the database" do
+        expect { post :create, deal: attributes_for(:invalid_deal)}.not_to change(Deal, :count)
+      end
+
+      it "redirects to the new deal path" do
+        post :create, deal: attributes_for(:invalid_deal)
+        expect(response).to redirect_to new_deal_path
+      end
+    end
   end
 
 end
 
-  # it "collects users into @users" do
-#     user = create(:user)
-#     get :index
-#     expect(assigns(:users)).to match_array [@user,user]
-#   end
-
-#   it "renders the :index template" do
-#     get :index
-#     expect(response).to render_template :index
-#   end
-# end
-
-# before :each do
-#   @user = create(:user)
-#   session[:user_id] = @user.id
-# end
