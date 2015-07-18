3c3ead67eec58ca87260f7ce0ffb6234aedd5e86
johnlyden <john.lyden6@gmail.com>
2015-07-18 12:08:22 -0400

    fixing invalid_deal instance in factory girl

--- spec/factories/deals.rb
+++ spec/factories/deals.rb
@@ -1,12 +1,14 @@
 FactoryGirl.define do
   factory :deal do
+
+    association :vendor
     title {Faker::Commerce.product_name}
     description {Faker::Lorem.sentence(3)}
     item_quantity {Faker::Number.number(2)}
     item_price {Faker::Commerce.price}
 
     factory :invalid_deal do
-      username nil
+      item_price nil
     end
   end
 end
