c9258c76fbab0a3dbc06ec56eafb19118f16753f
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-07-19 18:48:51 -0400

    update the purchase with new stripe data

--- app/controllers/purchased_items_controller.rb
+++ app/controllers/purchased_items_controller.rb
@@ -17,14 +17,14 @@ class PurchasedItemsController < ApplicationController
 
 
       customer = Stripe::Customer.create(
-        :email => 'example@stripe.com',
+        :email => current_user.email,
         :card  => params[:stripeToken]
       )
 
       charge = Stripe::Charge.create(
-        :customer    => customer.id,
+        :customer    => current_vendor.stripe_user_id,
         :amount      => calculate_stripe_amount(params[:quantity], deal),
-        :description => 'Rails Stripe customer',
+        :description => current_vendor.name,
         :currency    => 'usd'
       )
       flash[:success] = "Your purchase was completed successfully!"
