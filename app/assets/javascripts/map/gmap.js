d528bd2414b262d584615981b6c654040ea2f813
Marc Cardacci <marc.cardacci@gmail.com>
2015-07-19 18:58:19 -0400

    experiments with gmap

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -31,13 +31,13 @@ function toggleBounce() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
+// 48 Wall St New York, NY 10005
 
 //to pin drop on an address
 // map here is your google.map.Map element
 
   //   var myLatlng = new google.maps
-  //   geocoder.geocode({ 'address': addressThatYouWannaDraw }, function (results, status) {
+  //   Geocoder.geocode({ 'address': "48 Wall St New York, NY 10005" }, function (results, status) {
   //   if (status == google.maps.GeocoderStatus.OK) {
   //     pt = results[0].geometry.location;
 
@@ -49,20 +49,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
   //     var marker = new google.maps.Marker(myMarkerOpts);
   //   }
   // });
+//experiment
 
-//experiment!!
-// function initialize() {
-//   var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
-//   var mapOptions = {
-//     zoom: 8,
-//     center: myLatlng
-//   }
-//   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-//   var marker = new google.maps.Marker({
-//     position: myLatlng,
-//     map: map,
-//     title: 'Hello World!'
-//   });
-// }
 
-// google.maps.event.addDomListener(window, 'load', initialize);
+
