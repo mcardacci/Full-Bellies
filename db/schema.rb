e8c8249e844411a05d23077424785c3c95b1b800
John Lyden <john.lyden86@gmail.com>
2015-07-19 17:32:21 -0400

    adding paperclip avatar migration for vendor and profile pic partial

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719170445) do
+ActiveRecord::Schema.define(version: 20150719212959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,15 +60,19 @@ ActiveRecord::Schema.define(version: 20150719170445) do
   end
 
   create_table "vendors", force: :cascade do |t|
-    t.string   "username",        null: false
-    t.string   "password_digest", null: false
-    t.string   "email",           null: false
-    t.string   "name",            null: false
+    t.string   "username",            null: false
+    t.string   "password_digest",     null: false
+    t.string   "email",               null: false
+    t.string   "name",                null: false
     t.text     "bio"
-    t.string   "address",         null: false
+    t.string   "address",             null: false
     t.string   "phone_number"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
