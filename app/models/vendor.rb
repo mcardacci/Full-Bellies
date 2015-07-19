0d2824bf2dd79c407816dbc1af3e4bb53a707892
Lowell Mower <lowell.m.mower@gmail.com>
2015-07-19 09:53:18 -0400

    add phone parse method and filter for twilio

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -13,18 +13,27 @@ class Vendor < ActiveRecord::Base
   def send_followers_sms(deal)
     acct_sid = ENV["TWILIO_ACCT_SID"]
     auth_token = ENV["TWILIO_AUTH"]
+
+    @deal = deal
+    @vendor = Vendor.find(deal.vendor_id)
+    @phones = parse_numbers(@vendor.followers.pluck(:phone_number))
+    @names = @vendor.followers.pluck(:username)
     @client = Twilio::REST::Client.new acct_sid, auth_token
     from = '+12055442814'
-    followers = {'+16033400606' => 'Lowell'}
-    followers.each do |num, name|
+    # followers = {'+16033400606' => 'Lowell'}
+    @phones.each do |num|
       message = @client.account.messages.create(
         :from => from,
-        :to => num,
-        :body => "Hey, #{name} new deals avaiable!"
+        :to => '+1'+num,
+        :body => "Ahoy! #{@vendor.name} just posted they have #{@deal.title}! Click here to purchase."
         )
     end
   end
 
-end
+  def parse_numbers(phone_array)
+    phone_array.delete_if{|x| x == nil}
+    phone_array.map! {|x| x.scan(/\d+/).join }.delete_if{|x| x.length < 10  }
+    return phone_array
+  end
 
-.scan(/\d+/).join
+end
