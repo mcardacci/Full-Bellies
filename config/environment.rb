95a366adb0a19bd32c4c44ff0b64948f7ae2a974
Lowell Mower <lowell.m.mower@gmail.com>
2015-07-20 19:26:29 -0400

    change domain for mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SEND_GRID_USERNAME'],
   :password => ENV['SEND_GRID_PASS'],
-  :domain => 'http://localhost:3000',
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
