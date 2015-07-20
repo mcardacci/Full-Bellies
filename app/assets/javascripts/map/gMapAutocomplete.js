8ce3d53e782a225533638d280ea4dac427ff8344
Marc Cardacci <marc.cardacci@gmail.com>
2015-07-20 15:41:37 -0400

    adds autocomplete js to app

--- app/assets/javascripts/map/gMapAutocomplete.js
+++ app/assets/javascripts/map/gMapAutocomplete.js
@@ -1,8 +1,6 @@
-function initialize() {
-
-var input = document.getElementById('vendor_address');
-
-var autocomplete = new google.maps.places.Autocomplete(input);
+function init() {
+  var input = document.getElementById('vendor_address');
+  var autocomplete = new google.maps.places.Autocomplete(input);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', init);
