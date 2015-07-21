4894e4adf8910676be14ac00a700585fc74f81ed
Marc Cardacci <marc.cardacci@gmail.com>
2015-07-21 15:39:45 -0400

    set up before and after methods, first test passing

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,32 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, :type => :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    # ActionMailer::Base.delivery_method = :test
+    # ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
+    @vendor = create(:vendor)
+    @deal = create(:deal)
+    @vendor.deals << @deal
+    @user = create(:user)
+    @user.favorites << @vendor
+    UserMailer.email_followers(@deal).deliver
+  end
+
+  after(:each) do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  it "sends an email" do
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
+  end
+
+  it 'renders the receiver email' do
+    # expect(response)
+  end
+
+  it 'should set the title to the corerct title' do
+    expect(ActionMailer::Base.deliveries.first.title).to eq(@deal.title)
+  end
 end
+
