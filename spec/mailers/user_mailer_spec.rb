a05500d5d2e2ccca2d74f688f697ab24572495a4
Marc Cardacci <marc.cardacci@gmail.com>
2015-07-21 16:26:13 -0400

    adds a model test for email followers

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,37 +1,76 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, :type => :mailer do
-  before(:each) do
-    # ActionMailer::Base.delivery_method = :test
-    # ActionMailer::Base.perform_deliveries = true
-    ActionMailer::Base.deliveries = []
-    @deliveries = ActionMailer::Base.deliveries
-    @vendor = create(:vendor)
-    @deal = create(:deal)
-    @vendor.deals << @deal
-    @user = create(:user)
-    @user.favorites << @vendor
-    UserMailer.email_followers(@deal).deliver
+  describe "email_vendor" do
+    before(:each) do
+      ActionMailer::Base.deliveries = []
+      @deliveries = ActionMailer::Base.deliveries
+      @vendor = create(:vendor)
+      @deal = create(:deal)
+      @vendor.deals << @deal
+      @user = create(:user)
+      @user.favorites << @vendor
+      UserMailer.email_followers(@deal).deliver
+    end
+
+    after(:each) do
+      @deliveries.clear
+    end
+
+    it "sends an email" do
+      expect(@deliveries.count).to eq(1)
+    end
+
+    it 'renders the receiver name' do
+      expect(@deliveries.first.from).to eq(["fullbellies@example.com"])
+    end
+
+    it 'renders the subject of the email should contain the vendor name' do
+      expect(@deliveries.first.subject).to include(@vendor.name)
+    end
+
+    it 'should include a link to full bellies in the body' do
+      expect(@deliveries.first.body).to include("https://afternoon-meadow-3523.herokuapp.com/")
+    end
   end
 
-  after(:each) do
-    @deliveries.clear
-  end
+  describe "email_vendor" do
+    before(:each) do
+      ActionMailer::Base.deliveries = []
+      @deliveries = ActionMailer::Base.deliveries
+      @vendor = create(:vendor)
+      @deal = create(:deal)
+      @vendor.deals << @deal
+      @user = create(:user)
+      @user.favorites << @vendor
+      @purchase = create(:purchased_item)
+      UserMailer.email_vendor(@deal, @purchase).deliver
+    end
 
-  it "sends an email" do
-    expect(@deliveries.count).to eq(1)
-  end
+    it "sends an email" do
+      expect(@deliveries.count).to eq(1)
+    end
 
-  it 'renders the receiver name' do
-    expect(@deliveries.first.from).to eq(["fullbellies@example.com"])
-  end
+    it 'renders the receiver name' do
+      expect(@deliveries.first.from).to eq(["fullbellies@example.com"])
+    end
 
-  it 'renders the subject of the email should contain the vendor name' do
-    expect(@deliveries.first.subject).to include(@vendor.name)
-  end
+    it 'renders the subject of the email should contain the vendor name' do
+      expect(@deliveries.first.subject).to include(@vendor.name)
+    end
 
-  it 'should include a link to full bellies in the body' do
-    expect(@deliveries.first.body).to include("https://afternoon-meadow-3523.herokuapp.com/")
+    it 'should include a link to full bellies in the body' do
+      expect(@deliveries.first.body).to include("https://afternoon-meadow-3523.herokuapp.com/")
+    end
   end
 end
+
+
+
+
+
+
+
+
+
 
