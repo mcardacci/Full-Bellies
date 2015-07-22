4c146183b256e1f4c523a799fb897457561b03f0
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-07-22 09:46:01 -0400

    created purchased item test

--- spec/controllers/purchased_items_controller_spec.rb
+++ spec/controllers/purchased_items_controller_spec.rb
@@ -1,5 +1,21 @@
-require 'rails helper'
+require 'rails_helper'
 
-  describe 'Get #create' do
-    it "creates a charge"
+describe PurchasedItemsController do
+  before(:each) do
+    deal = create(:deal)
+    @vendor = create(:vendor)
+    @user = create(:user)
   end
+  describe 'POST #create' do
+    it "should return you to the current_user's profile" do
+      deal = create(:deal)
+      @vendor = create(:vendor)
+      current_user = create(:user)
+      session[:user_id] = current_user.id
+      Stripe.stub(:api_key)
+      Stripe::Charge.stub(:create)
+      post :create, deal_id: deal.id, quantity: 1
+      expect(response).to redirect_to user_path(current_user.id)
+    end
+  end
+end
